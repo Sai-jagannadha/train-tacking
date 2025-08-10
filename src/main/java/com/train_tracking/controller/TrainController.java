f8f5c24ac7b03a714feb6b52e7fa18b18543dc1c
Sai-jagannadha <saijagannadha.dev@gmail.com>
2025-08-10 21:30:59 +0530

    Added the test

--- src/main/java/com/train_tracking/controller/TrainController.java
+++ src/main/java/com/train_tracking/controller/TrainController.java
@@ -1,14 +1,17 @@
 package com.train_tracking.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/train")
 public class TrainController {
-    //build all the crud operations
-
-
+    @GetMapping("/test")
+    public String Test() {
+        return "Welcome to the train tracking application";
+    }
+    //build all the crud operationsc
 
 
 }
